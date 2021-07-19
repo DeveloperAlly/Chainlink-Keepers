9ec3c21e17856372de1a2c41cb2d52fce3fe9464
Alison Haire <alisonhaire@alisons-mbp.lan>
2021-07-19 22:20:20 +1000

    fix build errors

--- pages/components/StatusMessage.js
+++ pages/components/StatusMessage.js
@@ -1,40 +1,44 @@
 import React, { useEffect } from "react";
 import { Message, Icon } from "semantic-ui-react";
 
-const StatusMessage = ({
-  status: { error = "", loading = "", warning = "", success = "" },
-  ...props
-}) => {
+const StatusMessage = ({ status, ...props }) => {
+  const { error, success, warning, loading } = status;
   return (
-    <div>
-      <Message
-        compact
-        icon
-        negative={Boolean(error)}
-        success={Boolean(success) && !Boolean(loading)}
-        info={Boolean(loading)}
-        warning={Boolean(warning)}
-      >
-        <Icon
-          name={
-            loading
-              ? "circle notched"
-              : error
-              ? "times circle"
-              : success
-              ? "check circle"
-              : "exclamation circle"
-          }
-          loading={Boolean(loading)}
-        />
-        <Message.Content>
-          {Boolean(success) && !Boolean(loading) && (
-            <Message.Header>Transaction Success!</Message.Header>
-          )}
-          {loading ? loading : error ? error : success ? success : warning}
-        </Message.Content>
-      </Message>
-    </div>
+    <>
+      {status ? (
+        <div>
+          <Message
+            compact
+            icon
+            negative={Boolean(error)}
+            success={Boolean(success) && !Boolean(loading)}
+            info={Boolean(loading)}
+            warning={Boolean(warning)}
+          >
+            <Icon
+              name={
+                loading
+                  ? "circle notched"
+                  : error
+                  ? "times circle"
+                  : success
+                  ? "check circle"
+                  : "exclamation circle"
+              }
+              loading={Boolean(loading)}
+            />
+            <Message.Content>
+              {Boolean(success) && !Boolean(loading) && (
+                <Message.Header>Transaction Success!</Message.Header>
+              )}
+              {loading ? loading : error ? error : success ? success : warning}
+            </Message.Content>
+          </Message>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
   );
 };
 
